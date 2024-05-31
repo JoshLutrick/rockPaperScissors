f739f40cfa75096ec2503dd974dbe0f02bab4b94
JoshLutrick <joshslutrick@gmail.com>
2024-05-30 17:25:42 -0700

    Added some game logic

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const gameObj = {
+    gameContent: {
+        roundCapHTML: document.querySelector('.gameContainer_Content_roundCap'),
+        roundStartHTML: document.querySelector('.gameContainer_Content_roundStart'),
+        playerScoreHTML: document.querySelector('.gameContainer_Content_PlayerScore'),
+        computerScoreHTML: document.querySelector('.gameContainer_Content_ComputerScore')
+    },
     rounds: {
         roundStart: 0,
         roundCap: 5,
@@ -19,10 +25,42 @@ const gameObj = {
 
 const gameContent = {
     buttons: {
-        rock: $(".rock"),
-        paper: $(".paper"),
-        scissors: $('.scissors')
+        rock: document.querySelector('.rock'),
+        paper: document.querySelector('.paper'),
+        scissors: document.querySelector('.scissors'),
+        restart: document.querySelector('.restart'),
+        start: document.querySelector('.start')
     },
+    gameLogic: {
+        choices: {
+            rockChoice: 'Rock',
+            paperChoice: 'Paper',
+            scissorsChoice: 'Scissors'
+        }
+    }
+}
+
+function startGame(){
+    gameObj.gameContent.roundCapHTML.innerHTML = gameObj.rounds.roundCap
 }
 
 
+gameContent.buttons.start.addEventListener('click', startGame)
+
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    if(randomNum === 1){
+        return gameContent.gameLogic.choices.rockChoice
+    } else if (randomNum === 2){
+        return gameContent.gameLogic.choices.paperChoice
+    } else if (randomNum === 3){
+        return gameContent.gameLogic.choices.scissorsChoice
+    } else {
+        return 'ERROR'
+    }
+}
+
+function playerChoice(){
+    
+}
